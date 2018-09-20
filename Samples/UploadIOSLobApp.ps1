7c4a3c019f1c0d950bca6297311ca3fe50942005
Rohit Ramu <roramu@microsoft.com>
2018-09-20 16:03:17 -0700

    Add the initial Apps module with the LOB app upload cmdlet, as well as sample usage

---
+++ Samples/UploadIOSLobApp.ps1
@@ -0,0 +1,19 @@
+# Create the object that contains information about the app
+$appToUpload = New-MobileAppObject `
+    -iosLobApp `
+    -displayName 'Test' `
+    -description 'This is a test iOS LOB app' `
+    -publisher 'Rohit Ramu' `
+    -bundleId '' `
+    -applicableDeviceType (New-IosDeviceTypeObject -iPad $true -iPhoneAndIPod $true) `
+    -minimumSupportedOperatingSystem (New-IosMinimumOperatingSystemObject -v9_0 $true) `
+    -fileName 'test.ipa' `
+    -buildNumber 'v1' -versionNumber 'v1' -expirationDateTime ((Get-Date).AddDays(90))
+
+# Upload the app file with the app information
+# !! Replace 'test.ipa' with the path to your *.ipa file !!
+$createdApp = New-LobApp `
+    -filePath 'test.ipa' `
+    -mobileApp $appToUpload
+
+Write-Output $createdApp
