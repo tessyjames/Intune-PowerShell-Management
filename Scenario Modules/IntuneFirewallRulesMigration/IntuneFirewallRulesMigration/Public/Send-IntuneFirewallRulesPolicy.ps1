ac10fced8fe4343ce3e268d5a5ee59bb71d41965
Kevin Hui <t-kehui@microsoft.com>
2019-09-12 14:42:26 -0400

    added firewall rule migration tool

---
+++ Scenario Modules/IntuneFirewallRulesMigration/IntuneFirewallRulesMigration/Public/Send-IntuneFirewallRulesPolicy.ps1
@@ -0,0 +1,123 @@
+. "$PSScriptRoot\IntuneFirewallRule.ps1"
+. "$PSScriptRoot\..\Private\Send-Telemetry.ps1"
+. "$PSScriptRoot\..\Private\Use-HelperFunctions.ps1"
+. "$PSScriptRoot\..\Private\Strings.ps1"
+
+$ProfileFirewallRuleLimit = 150
+# Sends Intune Firewall objects out to the Intune Powershell SDK
+# and returns the response to the API call
+Function Send-IntuneFirewallRulesPolicy {
+    <#
+    .SYNOPSIS
+    Send firewall rule objects out to Intune
+
+    .DESCRIPTION
+    Sends IntuneFirewallRule objects out to the Intune Powershell SDK and returns the response to the API call
+
+    .EXAMPLE
+    Get-NetFirewallRule | ConvertTo-IntuneFirewallRule | Send-IntuneFirewallRulesPolicy
+    Send-IntuneFirewallRulesPolicy -firewallObjects $randomObjects
+    Get-NetFirewallRule -PolicyStore RSOP | ConvertTo-IntuneFirewallRule -splitConflictingAttributes | Send-IntuneFirewallRulesPolicy -migratedProfileName "someCustomName"
+    Get-NetFirewallRule -PolicyStore PersistentStore -PolicyStoreSourceType Local | ConvertTo-IntuneFirewallRule -sendConvertTelemetry | Send-IntuneFirewallRulesPolicy -migratedProfileName "someCustomName" -sendIntuneFirewallTelemetry $true
+
+    .PARAMETER firewallObjects the collection of firewall objects to be sent to be processed
+    .PARAMETER migratedProfileName an optional argument that represents the prefix for the name of newly created firewall rule profiles
+
+    .NOTES
+    While Send-IntuneFirewallRulesPolicy primarily accepts IntuneFirewallRule objects, any object piped into the cmdlet that can be
+    called with the ConvertTo-Json cmdlet and represented as a JSON string can be sent to Intune, with the Graph
+    performing the validation on the the JSON payload.
+
+    Users should authenticate themselves through the SDK first by running Connect-MSGraph, which will then allow
+    them to use this cmdlet.
+
+    Send-IntuneFirewallRulesPolicy ignores null or empty string values from objects, only passing valued attributes to Graph
+
+    .LINK
+    https://docs.microsoft.com/en-us/graph/api/resources/intune-deviceconfig-windowsfirewallrule?view=graph-rest-beta
+    #>
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    Param(
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        $firewallObjects,
+
+        [Parameter(Mandatory = $false)]
+        [String]
+        $migratedProfileName = $Strings.SendIntuneFirewallRulesPolicyProfileNameDefault,
+
+        # If this flag is toggled, then telemetry is automatically sent to Microsoft.
+        [switch]
+        $sendIntuneFirewallTelemetry
+    )
+
+    Begin { $firewallArr = @() }
+
+    # We apply a filter that strips objects of their null attributes so that Graph can
+    # apply default values in the absence of set values
+    Process {
+        $object = $_
+        $allProperties = $_.PsObject.Properties.Name
+        $nonNullProperties = $allProperties.Where( { $null -ne $object.$_ -and $object.$_ -ne "" })
+        $firewallArr += $object | Select-Object $nonNullProperties
+    }
+
+    End {
+        # Split the incoming firewall objects into separate profiles
+        $profiles = @()
+        $currentProfile = @()
+        ForEach ($firewall in $firewallArr) {
+            If ($currentProfile.Count -ge $ProfileFirewallRuleLimit) {
+                # Arrays may be "unrolled", so we need to enforce no unrolling
+                $profiles += , $currentProfile
+                $currentProfile = @()
+            }
+            $currentProfile += $firewall
+        }
+        If ($currentProfile.Count -gt 0 ) {
+            # Arrays may be "unrolled", so we need to enforce no unrolling
+            $profiles += , $currentProfile
+        }
+        $profileNumber = 0
+
+        $remainingProfiles = $profiles.Count
+        ForEach ($profile in $profiles) {
+            # remainingProfiles is decremented after displaying operation status
+            $remainingProfiles = Show-OperationProgress `
+                -remainingObjects $remainingProfiles `
+                -totalObjects $profiles.Count `
+                -activityMessage $Strings.SendIntuneFirewallRulesPolicyProgressStatus
+
+            # Processing a profile
+            $profileJson = $profile | ConvertTo-Json
+            $NewIntuneObject = "{
+                `"@odata.type`": `"#microsoft.graph.windows10EndpointProtectionConfiguration`",
+                `"displayName`": `"$migratedProfileName-$profileNumber`",
+                `"firewallRules`": $profileJson,
+                   }"
+            If ($PSCmdlet.ShouldProcess($NewIntuneObject, $Strings.SendIntuneFirewallRulesPolicyShouldSendData)) {
+                Try {
+                    Invoke-MSGraphRequest -Url 'https://graph.microsoft.com/beta/devicemanagement/deviceconfigurations/' -HttpMethod POST -Content $NewIntuneObject
+                    # Profile numbers will only increment if they are actually sent to Intune
+                    $profileNumber++
+                }
+                Catch {
+                    # Intune Graph errors are telemetry points that can detect payload mistakes
+                    $errorMessage = $_.ToString()
+                    $errorType = $_.Exception.GetType().ToString()
+                    $choice = Get-IntuneFirewallRuleErrorTelemetryChoice -telemetryMessage $errorMessage `
+                        -sendErrorTelemetryInitialized $sendIntuneFirewallTelemetry `
+                        -telemetryExceptionType $errorType
+                    switch ($choice) {
+                        "Yes" { Send-IntuneFirewallGraphTelemetry -data $errorMessage }
+                        "No" { Throw $Strings.SendIntuneFirewallRulesPolicyException }
+                        "Yes To All" {
+                            Send-IntuneFirewallGraphTelemetry -data $errorMessage
+                            $sendIntuneFirewallTelemetry = $true
+                        }
+                        "Continue" { continue }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Scenario Modules/IntuneFirewallRulesMigration/Tests/Process-IntuneFirewallRules.Tests.ps1
@@ -0,0 +1,212 @@
+. "$PSScriptRoot\..\IntuneFirewallRulesMigration\Public\IntuneFirewallRule.ps1"
+. "$PSScriptRoot\..\IntuneFirewallRulesMigration\Private\Process-IntuneFirewallRules.ps1"
+
+Describe "Test-IntuneFirewallRuleSplit" {
+    It "Should return True if serviceName, filePath, and packageFamilyName are filled" {
+        $firewallRule = New-IntuneFirewallRule
+        $firewallRule.packageFamilyName = "packageFamilyNameFoo"
+        $firewallRule.filePath = "filePathFoo"
+        $firewallRule.serviceName = "serviceNameFoo"
+
+        Test-IntuneFirewallRuleSplit -firewallObject $firewallRule | Should -BeTrue
+    }
+
+    It "Should return True if filePath, and packageFamilyName are filled" {
+        $firewallRule = New-IntuneFirewallRule
+        $firewallRule.packageFamilyName = "packageFamilyNameFoo"
+        $firewallRule.filePath = "filePathFoo"
+
+        Test-IntuneFirewallRuleSplit -firewallObject $firewallRule | Should -BeTrue
+    }
+
+    It "Should return True if serviceName and packageFamilyName are filled" {
+        $firewallRule = New-IntuneFirewallRule
+        $firewallRule.packageFamilyName = "packageFamilyNameFoo"
+        $firewallRule.serviceName = "serviceNameFoo"
+
+        Test-IntuneFirewallRuleSplit -firewallObject $firewallRule | Should -BeTrue
+    }
+
+    It "Should return True if serviceName, filePath are filled" {
+        $firewallRule = New-IntuneFirewallRule
+        $firewallRule.filePath = "filePathFoo"
+        $firewallRule.serviceName = "serviceNameFoo"
+
+        Test-IntuneFirewallRuleSplit -firewallObject $firewallRule | Should -BeTrue
+    }
+
+    It "Should return False if only serviceName is filled" {
+        $firewallRule = New-IntuneFirewallRule
+        $firewallRule.serviceName = "serviceNameFoo"
+
+        Test-IntuneFirewallRuleSplit -firewallObject $firewallRule | Should -BeFalse
+    }
+
+    It "Should return False if only packageFamilyName is filled" {
+        $firewallRule = New-IntuneFirewallRule
+        $firewallRule.packageFamilyName = "packageFamilyNameFoo"
+
+        Test-IntuneFirewallRuleSplit -firewallObject $firewallRule | Should -BeFalse
+    }
+
+    It "Should return False if only filePath is filled" {
+        $firewallRule = New-IntuneFirewallRule
+        $firewallRule.filePath = "filePathFoo"
+
+        Test-IntuneFirewallRuleSplit -firewallObject $firewallRule | Should -BeFalse
+    }
+
+    It "Should return False if none are filled" {
+        $firewallRule = New-IntuneFirewallRule
+
+        Test-IntuneFirewallRuleSplit -firewallObject $firewallRule | Should -BeFalse
+    }
+}
+
+Describe "Split-IntuneFirewallRule" {
+    It "Should split firewall object into three objects if serviceName, filePath, and packageFamilyName are filled" {
+        $firewallRule = New-IntuneFirewallRule
+        $firewallRule.packageFamilyName = "packageFamilyNameFoo"
+        $firewallRule.filePath = "filePathFoo"
+        $firewallRule.serviceName = "serviceNameFoo"
+
+        Split-IntuneFirewallRule -firewallObject $firewallRule | Should -HaveCount 3
+    }
+
+    It "Should split firewall object into two objects if serviceName and filePath are filled" {
+        $firewallRule = New-IntuneFirewallRule
+        $firewallRule.filePath = "filePathFoo"
+        $firewallRule.serviceName = "serviceNameFoo"
+
+        Split-IntuneFirewallRule -firewallObject $firewallRule | Should -HaveCount 2
+    }
+
+    It "Should split firewall object into two objects if packageFamilyName and filePath are filled" {
+        $firewallRule = New-IntuneFirewallRule
+        $firewallRule.packageFamilyName = "packageFamilyNameFoo"
+        $firewallRule.filePath = "filePathFoo"
+
+        Split-IntuneFirewallRule -firewallObject $firewallRule | Should -HaveCount 2
+    }
+
+    It "Should split firewall object into two objects if packageFamilyName and serviceName are filled" {
+        $firewallRule = New-IntuneFirewallRule
+        $firewallRule.packageFamilyName = "packageFamilyNameFoo"
+        $firewallRule.serviceName = "serviceNameFoo"
+
+        Split-IntuneFirewallRule -firewallObject $firewallRule | Should -HaveCount 2
+    }
+
+    It "Should split firewall object into one object if packageFamilyName is filled" {
+        $firewallRule = New-IntuneFirewallRule
+        $firewallRule.packageFamilyName = "packageFamilyNameFoo"
+
+        Split-IntuneFirewallRule -firewallObject $firewallRule | Should -HaveCount 1
+    }
+
+    It "Should split firewall object into one object if filePath is filled" {
+        $firewallRule = New-IntuneFirewallRule
+        $firewallRule.filePath = "filePathFoo"
+
+        Split-IntuneFirewallRule -firewallObject $firewallRule | Should -HaveCount 1
+    }
+
+    It "Should split firewall object into one object if serviceName is filled" {
+        $firewallRule = New-IntuneFirewallRule
+        $firewallRule.serviceName = "serviceNameFoo"
+
+        Split-IntuneFirewallRule -firewallObject $firewallRule | Should -HaveCount 1
+    }
+
+    It "should return nothing if no desired property is filled" {
+        $firewallRule = New-IntuneFirewallRule
+
+        Split-IntuneFirewallRule -firewallObject $firewallRule | Should -HaveCount 0
+    }
+}
+
+Describe "Copy-IntuneFirewallRule" {
+    It "Should copy expected attributes from firewall object" {
+        $oldFirewallRule = New-IntuneFirewallRule
+        $oldFirewallRule.displayName = "displayNameFoo"
+        $oldFirewallRule.description = "descriptionFoo"
+        $oldFirewallRule.packageFamilyName = "packageFamilyNameFoo"
+        $oldFirewallRule.serviceName = "serviceNameFoo"
+        $oldFirewallRule.protocol = 2
+        $oldFirewallRule.localPortRanges = @("foo", "bar", "baz")
+        $oldFirewallRule.remotePortRanges = @("remoteFoo")
+        $oldFirewallRule.localAddressRanges = @("localAddr", "foo")
+        $oldFirewallRule.remoteAddressRanges = @("remoteAddr", "bar", "baz")
+        $oldFirewallRule.profileTypes = "profileTypesFoo"
+        $oldFirewallRule.action = "actionFoo"
+        $oldFirewallRule.trafficDirection = "trafficDirectionFoo"
+        $oldFirewallRule.interfaceTypes = "interfaceTypesFoo"
+        $oldFirewallRule.localUserAuthorizations = "localUserAuthFoo"
+        $oldFirewallRule.edgeTraversal = "edgeTraversalFoo"
+
+        $newFirewall = Copy-IntuneFirewallRule -firewallObject $oldFirewallRule
+        ForEach ($property in $newFirewall.PSObject.Properties) {
+            $property.Value | Should -Be $oldFirewallRule.($property.Name)
+        }
+    }
+
+    It "Should not cause a copied object's strings to change if changing an attribute" {
+        $oldFirewallRule = New-IntuneFirewallRule
+        $oldFirewallRule.displayName = "displayNameFoo"
+        $newFirewall = Copy-IntuneFirewallRule -firewallObject $oldFirewallRule
+
+        $oldFirewallRule.displayName = "somethingDifferent"
+        $oldFirewallRule.displayName | Should -Be "somethingDifferent"
+        $newFirewall.displayName | Should -Be "displayNameFoo"
+    }
+
+    It "Should not cause a copied object's string arrays to change if changing to a new array" {
+        $oldFirewallRule = New-IntuneFirewallRule
+        $oldFirewallRule.localPortRanges = @("foo", "bar", "baz")
+        $newFirewall = Copy-IntuneFirewallRule -firewallObject $oldFirewallRule
+
+        $oldFirewallRule.localPortRanges = @("newArr")
+        $oldFirewallRule.localPortRanges | Should -Be @("newArr")
+        $newFirewall.localPortRanges | Should -Be @("foo", "bar", "baz")
+    }
+
+    It "Should mutate a copied object's array if mutating their own array" {
+        $oldFirewallRule = New-IntuneFirewallRule
+        $oldFirewallRule.localPortRanges = @("foo", "bar", "baz")
+        $newFirewall = Copy-IntuneFirewallRule -firewallObject $oldFirewallRule
+
+        $oldFirewallRule.localPortRanges[0] = "new"
+        $newFirewall.localPortRanges | Should -Be @("new", "bar", "baz") -Because "Object is shallow copied"
+    }
+}
+
+Describe "Get-SplitIntuneFirewallRuleChoice" {
+    It "Should return 'Yes' if given the -splitConflictingAttributes flag with a true value" {
+        $foo=New-IntuneFirewallRule
+        Get-SplitIntuneFirewallRuleChoice -firewallObject $foo -splitConflictingAttributes $true | Should -Be "Yes"
+    }
+
+    It "Should return 'Yes' if user selected 'Yes'" {
+        $foo=New-IntuneFirewallRule
+        Mock Get-UserPrompt -MockWith { return 0 }
+        Get-SplitIntuneFirewallRuleChoice -firewallObject $foo | Should -Be "Yes"
+    }
+
+    It "Should return 'No' if user selected 'No'" {
+        $foo=New-IntuneFirewallRule
+        Mock Get-UserPrompt -MockWith { return 1 }
+        Get-SplitIntuneFirewallRuleChoice -firewallObject $foo | Should -Be "No"
+    }
+
+    It "Should return 'Yes to All' if user selected 'Yes to All'" {
+        $foo=New-IntuneFirewallRule
+        Mock Get-UserPrompt -MockWith { return 2 }
+        Get-SplitIntuneFirewallRuleChoice -firewallObject $foo | Should -Be "Yes to All"
+    }
+
+    It "Should return 'Continue' if user selected 'Continue'" {
+        $foo=New-IntuneFirewallRule
+        Mock Get-UserPrompt -MockWith { return 3 }
+        Get-SplitIntuneFirewallRuleChoice -firewallObject $foo | Should -Be "Continue"
+    }
+}
